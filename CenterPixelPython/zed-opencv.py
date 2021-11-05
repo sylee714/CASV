792f8a483494014dcd007055be490a97977a9b4a
Seungyun Lee <sylee714@gmail.com>
2021-11-05 01:30:41 -0700

    finished the Python ver of center pixel checker

---
+++ CenterPixelPython/zed-opencv.py
@@ -0,0 +1,211 @@
+import sys
+import numpy as np
+import pyzed.sl as sl
+import cv2
+
+help_string = "[s] Save side by side image [d] Save Depth, [n] Change Depth format, [p] Save Point Cloud, [m] Change Point Cloud format, [q] Quit"
+prefix_point_cloud = "Cloud_"
+prefix_depth = "Depth_"
+path = "./"
+
+count_save = 0
+mode_point_cloud = 0
+mode_depth = 0
+point_cloud_format_ext = ".ply"
+depth_format_ext = ".png"
+
+def point_cloud_format_name(): 
+    global mode_point_cloud
+    if mode_point_cloud > 3:
+        mode_point_cloud = 0
+    switcher = {
+        0: ".xyz",
+        1: ".pcd",
+        2: ".ply",
+        3: ".vtk",
+    }
+    return switcher.get(mode_point_cloud, "nothing") 
+  
+def depth_format_name(): 
+    global mode_depth
+    if mode_depth > 2:
+        mode_depth = 0
+    switcher = {
+        0: ".png",
+        1: ".pfm",
+        2: ".pgm",
+    }
+    return switcher.get(mode_depth, "nothing") 
+
+def save_point_cloud(zed, filename) :
+    print("Saving Point Cloud...")
+    tmp = sl.Mat()
+    zed.retrieve_measure(tmp, sl.MEASURE.XYZRGBA)
+    saved = (tmp.write(filename + point_cloud_format_ext) == sl.ERROR_CODE.SUCCESS)
+    if saved :
+        print("Done")
+    else :
+        print("Failed... Please check that you have permissions to write on disk")
+
+def save_depth(zed, filename) :
+    print("Saving Depth Map...")
+    tmp = sl.Mat()
+    zed.retrieve_measure(tmp, sl.MEASURE.DEPTH)
+    saved = (tmp.write(filename + depth_format_ext) == sl.ERROR_CODE.SUCCESS)
+    if saved :
+        print("Done")
+    else :
+        print("Failed... Please check that you have permissions to write on disk")
+
+def save_sbs_image(zed, filename) :
+
+    image_sl_left = sl.Mat()
+    zed.retrieve_image(image_sl_left, sl.VIEW.LEFT)
+    image_cv_left = image_sl_left.get_data()
+
+    image_sl_right = sl.Mat()
+    zed.retrieve_image(image_sl_right, sl.VIEW.RIGHT)
+    image_cv_right = image_sl_right.get_data()
+
+    sbs_image = np.concatenate((image_cv_left, image_cv_right), axis=1)
+
+    cv2.imwrite(filename, sbs_image)
+    
+
+def process_key_event(zed, key) :
+    global mode_depth
+    global mode_point_cloud
+    global count_save
+    global depth_format_ext
+    global point_cloud_format_ext
+
+    if key == 100 or key == 68:
+        save_depth(zed, path + prefix_depth + str(count_save))
+        count_save += 1
+    elif key == 110 or key == 78:
+        mode_depth += 1
+        depth_format_ext = depth_format_name()
+        print("Depth format: ", depth_format_ext)
+    elif key == 112 or key == 80:
+        save_point_cloud(zed, path + prefix_point_cloud + str(count_save))
+        count_save += 1
+    elif key == 109 or key == 77:
+        mode_point_cloud += 1
+        point_cloud_format_ext = point_cloud_format_name()
+        print("Point Cloud format: ", point_cloud_format_ext)
+    elif key == 104 or key == 72:
+        print(help_string)
+    elif key == 115:
+        save_sbs_image(zed, "ZED_image" + str(count_save) + ".png")
+        count_save += 1
+    else:
+        a = 0
+
+def print_help() :
+    print(" Press 's' to save Side by side images")
+    print(" Press 'p' to save Point Cloud")
+    print(" Press 'd' to save Depth image")
+    print(" Press 'm' to switch Point Cloud format")
+    print(" Press 'n' to switch Depth format")
+
+
+def main() :
+
+    # Create a ZED camera object
+    zed = sl.Camera()
+
+    # Set configuration parameters
+    input_type = sl.InputType()
+    if len(sys.argv) >= 2 :
+        input_type.set_from_svo_file(sys.argv[1])
+    init = sl.InitParameters(input_t=input_type)
+    init.camera_resolution = sl.RESOLUTION.HD720
+    init.depth_mode = sl.DEPTH_MODE.PERFORMANCE
+    init.coordinate_units = sl.UNIT.METER
+    init.depth_minimum_distance = 0.3
+    init.depth_maximum_distance = 2
+
+    # Open the camera
+    err = zed.open(init)
+    if err != sl.ERROR_CODE.SUCCESS :
+        print(repr(err))
+        zed.close()
+        exit(1)
+
+    # Display help in console
+    print_help()
+
+    # Set runtime parameters after opening the camera
+    runtime = sl.RuntimeParameters()
+    runtime.sensing_mode = sl.SENSING_MODE.STANDARD
+
+    # Prepare new image size to retrieve half-resolution images
+    image_size = zed.get_camera_information().camera_resolution
+    # image_size.width = image_size.width /2
+    # image_size.height = image_size.height /2
+    image_size.width = image_size.width
+    image_size.height = image_size.height
+
+    # Declare your sl.Mat matrices
+    image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
+    depth_image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
+    point_cloud = sl.Mat()
+    depth_map = sl.Mat()
+
+    key = ' '
+    while key != 113 :
+        err = zed.grab(runtime)
+        if err == sl.ERROR_CODE.SUCCESS :
+            # Retrieve the left image, depth image in the half-resolution
+            zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, image_size)
+            zed.retrieve_image(depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, image_size)
+            # Retrieve the RGBA point cloud in half resolution
+            zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA, sl.MEM.CPU, image_size)
+
+            # To recover data from sl.Mat to use it with opencv, use the get_data() method
+            # It returns a numpy array that can be used as a matrix with opencv
+            image_ocv = image_zed.get_data()
+            depth_image_ocv = depth_image_zed.get_data()
+
+            # draw in the image
+            # cv2.rectangle(image, start_point, end_point, color, thickness)
+            rect_width = 60
+            rect_height = 40
+            center_x = int(image_size.width/2)
+            center_y = int(image_size.height/2)
+            upper_left = (center_x - rect_width, center_y - rect_height)
+            bottom_right = (center_x + rect_width, center_y + rect_height)
+            cv2.rectangle(image_ocv, upper_left, bottom_right, (0, 255, 0), thickness=1)
+            cv2.rectangle(depth_image_ocv, upper_left, bottom_right, (0, 255, 0), thickness=1)
+
+            cv2.imshow("Image", image_ocv)
+            cv2.imshow("Depth", depth_image_ocv)
+
+            # Reference
+            # https://www.stereolabs.com/docs/opencv/python/
+            # Retrieve depth
+            zed.retrieve_measure(depth_map, sl.MEASURE.DEPTH)
+            # Load depth data into a numpy array
+            depth_ocv = depth_map.get_data()
+            # Print the depth value at the center of the image
+            print("Center Depth Value: ", depth_ocv[int(len(depth_ocv)/2)][int(len(depth_ocv[0])/2)])
+            
+            # The shape of depth_image_ocv is height x width x 4,
+            # where the '4' tells the color code [0-255, 0-255, 0-255, 255].
+            # After running some sample runs, the first 3 are nearly identical values 
+            # while the last one remains at 255.
+            # So, just getting the first value will tell the pixel value of the depth_image_ocv
+            print("Center Pixel Value: ", depth_image_ocv[int(len(depth_image_ocv)/2)][int(len(depth_image_ocv[0])/2)][0])
+
+
+            key = cv2.waitKey(10)
+
+            process_key_event(zed, key)
+
+    cv2.destroyAllWindows()
+    zed.close()
+
+    print("\nFINISH")
+
+if __name__ == "__main__":
+    main()
